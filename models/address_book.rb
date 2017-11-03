59ac7528861ddcf2f4d7c8a9b07e8e5c4932d17d
Adan Amarillas <aamarill.engr@gmail.com>
2017-11-03 10:24:20 -0700

    Add menu and menu controller

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,30 +9,24 @@ class AddressBook
 	end
 
 	def add_entry(name, phone_number, email)
-		# #9
 		index = 0
 		entries.each do |entry|
-		# #10
 			if name < entry.name
 				break
 			end
 			index += 1
 		end
-		# #11
 		entries.insert(index, Entry.new(name, phone_number, email))
 	end
 
 	def remove_entry(name, phone_number, email)
-
 		index = 0
 		entries.each do |entry|
-
 			if name === entry.name && phone_number === entry.phone_number && email === entry.email
 				break
 			end
 			index += 1
 		end
-
 		entries.delete_at(index)
 	end
 end
